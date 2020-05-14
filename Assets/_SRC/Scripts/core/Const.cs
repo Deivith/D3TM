70a590fb9a02a8c34bff3b564f6b1e74a170b09b
deivith <suarez.david@gmail.com>
2020-05-14 08:15:41 -0300

    Implementation of Animation

---
+++ Assets/_SRC/Scripts/core/Const.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.odaclick.d3.core {
+
+    public class Const {
+
+        public static class TAG {
+            public static string MAIN = "Main";
+        }
+
+    }
+
+
+}
---
+++ Assets/_SRC/Scripts/ui/UIAnimation.cs
@@ -0,0 +1,28 @@
+﻿using com.odaclick.d3.core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAnimation : MonoBehaviour
+{
+    private float yOffset = 0;
+
+    private void Start() {
+        Canvas[] canvas = GameObject.FindObjectsOfType<Canvas>();
+        for(int i = 0; i < canvas.Length; i++) {
+            if (canvas[i].tag.Equals(Const.TAG.MAIN)) {
+                yOffset = canvas[i].GetComponent<RectTransform>().rect.height;
+            }
+        }
+    }
+    public void EnableWindow() {
+        gameObject.SetActive(true);
+        LeanTween.move(GetComponent<RectTransform>(), new Vector3(0, 0, 0), 1f).setEaseInOutBack();
+    }
+    public void DisableWindow() {
+        LeanTween.move(GetComponent<RectTransform>(), new Vector3(0, yOffset, 0), 1f).setEaseInOutBack().setOnComplete(()=> {
+            gameObject.SetActive(false);
+        });
+    }
+
+}
